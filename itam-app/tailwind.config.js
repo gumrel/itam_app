a6311e44f6d9a6eefc407fb6844b52f4000f744f
gumrel <gumrel@MacBook-Air-gumrel.local>
2024-12-04 16:24:15 -0800

    header

--- itam-app/tailwind.config.js
+++ itam-app/tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
 	content: ['./src/components/**/*.{vue,js,ts}', './src/layouts/**/*.vue', './src/pages/**/*.vue', './src/app.vue'],
 	theme: {
 		extend: {},
+		screens: {
+			xs: '480px',
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1440px',
+			'2xl': '1536px',
+			'3xl': '1600px',
+		},
 	},
 	plugins: [require('daisyui')],
 	daisyui: {
@@ -15,7 +24,7 @@ module.exports = {
 					accent: '#84cc16',
 					neutral: '#6b7280',
 					'base-100': '#1118',
-					info: '#38bdf8',
+					info: '#6300DB',
 					success: '#22c55e',
 					warning: '#facc15',
 					error: '#f43f5e',
@@ -28,7 +37,7 @@ module.exports = {
 					accent: '#ff4500',
 					neutral: '#0f1516',
 					'base-100': '#f3f4f6',
-					info: '#00d5ff',
+					info: '#6300DB',
 					success: '#00eab9',
 					warning: '#f4be00',
 					error: '#e04662',
