deb2e6a8d7365bc5f609cd4f68eb1371d90ac993
gumrel <gumrel@MacBook-Air-gumrel.local>
2024-12-11 06:51:47 -0800

    rebase

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-
-// Маршруты для авторизации
-router.post("/login", authController.login);
-router.post("/register", authController.register);
-
-module.exports = router;
